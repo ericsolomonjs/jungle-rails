80d7bf2d48da54bc5dce11212243fc71121b308c
Eric Solomon <eric.solomon.ca@gmail.com>
2023-01-26 12:30:19 -0500

    implement feature admin dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV.fetch("ADMIN_USER"), password: ENV.fetch("ADMIN_PASSWORD")
   def show
+    @categories = Category.count()
+    @products = Product.count()
   end
+
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,8 +1,8 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: ENV.fetch("ADMIN_USER"), password: ENV.fetch("ADMIN_PASSWORD"), except: :index
+  http_basic_authenticate_with name: ENV.fetch("ADMIN_USER"), password: ENV.fetch("ADMIN_PASSWORD")
 
   def index
-    render plain: "Restricted area"
+    @products = Product.all()
   end
   
   def edit
